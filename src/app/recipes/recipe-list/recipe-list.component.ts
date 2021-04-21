f3c8cef544a900b7c6812635ed9eab09a80e53e4
lrivera <lrivera@udtonline.com>
2021-04-21 10:51:08 -0500

    Applying routing in all app

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -9,10 +10,11 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) { 
+  constructor(private recipeService: RecipeService,
+              private router: Router,
+              private route: ActivatedRoute) { 
   }
 
   ngOnInit() {
@@ -20,8 +22,7 @@ export class RecipeListComponent implements OnInit {
     this.recipes = this.recipeService.getRecipes(); 
   }
 
-  onRecipeSelected(recipe: Recipe) {
-   this.recipeWasSelected.emit(recipe);
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route });
   }
-
 }
